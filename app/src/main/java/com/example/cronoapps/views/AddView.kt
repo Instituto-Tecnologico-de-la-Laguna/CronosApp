9d583dc15379a49ea218febfed205102c583266d
jmot4 <jmota@gtech-services.com>
2025-05-14 13:06:02 -0600

    Se agrega navegacion

---
+++ app/src/main/java/com/example/cronoapps/views/AddView.kt
@@ -0,0 +1,10 @@
+package com.example.cronoapps.views
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableTarget
+import androidx.navigation.NavController
+
+@Composable
+fun AddView(navController: NavController){
+
+}
---
+++ app/src/main/java/com/example/cronoapps/views/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.cronoapps.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.compose.material3.Text
+@Composable
+fun HomeView(navController: NavController){
+    Text("Hola")
+}
