e529aac5ae625e36a7f710feb61233ce847fcaed
jmot4 <jmota@gtech-services.com>
2025-05-16 13:53:13 -0600

    Se agrega clase para control de estado de los cronometros y nombres de cada uno

--- app/src/main/java/com/example/cronoapps/MainActivity.kt
+++ app/src/main/java/com/example/cronoapps/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-
                 ) {
                     NavManager()
                 }
